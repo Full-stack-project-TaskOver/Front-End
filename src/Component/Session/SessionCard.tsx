4748166d4d940d2e2063950bf507a7262dcb8497
Rayan Jowair <rayanaljowair@gmail.com>
2023-03-11 06:46:54 +0300

    update sessions page

--- src/Component/Session/SessionCard.tsx
+++ src/Component/Session/SessionCard.tsx
@@ -10,33 +10,59 @@ import {
   Text,
   Icon,
   Flex,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import React from "react";
 import { MdFamilyRestroom, MdLeaderboard } from "react-icons/md";
 function SessionCard() {
   return (
-    <Card maxW="18em">
-      <CardBody>
+    <Card
+      maxW="18em"
+      backgroundColor={useColorModeValue("#FFDDD2", "white")}
+      shadow="sm"
+      borderRadius={15}
+      cursor="pointer"
+      transition={"200ms"}
+      _hover={{
+        backgroundColor: useColorModeValue("#90E1DE", "white"),
+      }}>
+      <CardBody pb={0.5}>
         <Stack spacing="2">
-          <Icon
-            as={MdFamilyRestroom}
-            boxSize={"2.5em"}
-            alignSelf="center"></Icon>
-          <Heading size="sm">Rayan Family</Heading>
-          <Text>Here we have all of our family tasks and goals</Text>
+          <Icon as={MdFamilyRestroom} boxSize={"2em"} color="gray.700"></Icon>
+          <Text
+            as={"h3"}
+            fontSize={{ base: "1rem", md: "1rem" }}
+            fontWeight="600">
+            Rayan Family
+          </Text>
+          <Text fontSize={{ base: "0.7rem", md: "0.8rem" }}>
+            Here we have all of our family tasks and goals
+          </Text>
         </Stack>
       </CardBody>
-      {/* <Divider /> */}
-      <CardFooter padding={2} justifyContent="center">
+      <CardFooter py={1.5} justifyContent="end">
         <ButtonGroup spacing="2">
-          <Button variant="solid" color="black" backgroundColor={"eee"}>
-            <Icon as={MdLeaderboard} />
-          </Button>
           <Button
+            py={0.5}
+            h="2.5em"
             variant="solid"
             color="black"
             fontSize={"0.8em"}
-            backgroundColor={"eee"}>
+            backgroundColor={useColorModeValue("#FCF2DB", "white")}>
+            {/* Leaderboard Icon */}
+            <Icon
+              as={MdLeaderboard}
+              color={useColorModeValue("black", "white")}
+              boxSize="1.3em"
+            />
+          </Button>
+          <Button
+            py={0.5}
+            h="2.5em"
+            variant="solid"
+            color="black"
+            fontSize={"0.8em"}
+            backgroundColor={useColorModeValue("#FCF2DB", "white")}>
             View
           </Button>
         </ButtonGroup>
--- src/Component/Session/SessionsIndex.tsx
+++ src/Component/Session/SessionsIndex.tsx
@@ -1,25 +1,85 @@
-import { Flex, Heading, HStack } from "@chakra-ui/react";
+import {
+  Box,
+  Card,
+  Divider,
+  Flex,
+  Grid,
+  Heading,
+  HStack,
+  Icon,
+  SimpleGrid,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import React from "react";
+import { AiOutlinePlus, AiOutlinePlusCircle } from "react-icons/Ai";
 import SessionCard from "./SessionCard";
+
+function AddSessionCard() {
+  return (
+    <Card
+      as={"button"}
+      maxW="18em"
+      backgroundColor={useColorModeValue("none", "white")}
+      shadow="sm"
+      borderRadius={15}
+      border="2px dashed gray"
+      cursor="pointer"
+      transition={"200ms"}
+      justifyContent="center"
+      _hover={{
+        backgroundColor: useColorModeValue("gray.100", "white"),
+      }}>
+      <Icon
+        as={AiOutlinePlus}
+        boxSize={"3em"}
+        alignSelf="center"
+        color={"gray"}
+      />
+    </Card>
+  );
+}
 
 function SessionsIndex() {
   return (
     <>
-      <Heading
-        as={"h1"}
-        fontSize={{ md: "2rem" }}
-        border="1px black solid"
-        pb="1.5em">
+      <Heading as={"h1"} fontSize={{ md: "2rem" }} pb="1.5em">
         Sessions
       </Heading>
-      <Flex w="80%" border="1px black solid" flexDirection="column">
-        <Flex border="1px black solid" flexDirection="column">
-          <Heading as="h2" fontSize={{ md: "1.3rem" }} pb="0.5em">
+      <Flex w="full" flexDirection="column">
+        {/* As Member Section */}
+        <Flex flexDirection={"column"} w="full">
+          <Heading as="h2" fontSize={{ md: "1.1rem" }} p="0.5em">
             As Member
           </Heading>
-          <Flex border="1px black solid" wrap="wrap" flexDirection="row">
+          <SimpleGrid
+            columns={{ base: 1, sm: 2, md: 3 }}
+            rowGap={3}
+            columnGap={3}
+            w="full">
             <SessionCard />
-          </Flex>
+            <SessionCard />
+            <SessionCard />
+            <SessionCard />
+            <AddSessionCard />
+          </SimpleGrid>
+        </Flex>
+        <Divider padding={2} />
+        {/* As Admin Section */}
+        <Flex flexDirection={"column"} w="full">
+          <Heading as="h2" fontSize={{ md: "1.1rem" }} p="0.5em">
+            As Admin
+          </Heading>
+          <SimpleGrid
+            columns={{ base: 1, sm: 2, md: 3 }}
+            rowGap={3}
+            columnGap={3}
+            w="full">
+            <SessionCard />
+            <SessionCard />
+            <SessionCard />
+            <SessionCard />
+            <AddSessionCard />
+          </SimpleGrid>
         </Flex>
       </Flex>
     </>
