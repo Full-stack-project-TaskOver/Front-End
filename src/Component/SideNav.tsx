bb85da5f78f5ece790bfdd93757082afcbbaa04e
Rayan Jowair <rayanaljowair@gmail.com>
2023-03-19 06:11:48 +0300

    update sideNav logo

--- src/Component/SideNav.tsx
+++ src/Component/SideNav.tsx
@@ -49,7 +49,7 @@ import { Route, Link, useLocation, useNavigate } from "react-router-dom";
 import { ValueTarget } from "framer-motion";
 import DarkModeIconButton from "./Tasks/DarkModeIcon";
 import Buttons from "./Authentication/Buttons";
-import logo from "../assets/logo.svg";
+import logo from "../assets/logo3.svg";
 
 interface LinkItemProps {
   name: string;
@@ -116,7 +116,7 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
       zIndex={10}
       {...rest}>
       <Flex h="20" alignItems="center" mx="3">
-        <Image src={logo} maxH={"10rem"} maxW="8rem"></Image>
+        <Image src={logo} maxH={"5rem"} maxW="5rem"></Image>
         <Spacer />
         <CloseButton display={{ base: "flex", md: "none" }} onClick={onClose} />
       </Flex>
@@ -204,8 +204,8 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
 
       <Image
         src={logo}
-        maxH={"10rem"}
-        maxW="8rem"
+        maxH={"5rem"}
+        maxW="5rem"
         display={{ base: "flex", md: "none" }}></Image>
 
       <Buttons />
